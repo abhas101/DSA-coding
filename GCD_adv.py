48d2f41d5aae20f0a2556881bf91abbc63071d75
Abhas Kumar -X (abhaskum - CAPGEMINI AMERICA INC at Cisco) <abhaskum@cisco.com>
2026-04-12 15:09:35 +0530

    added GCD code

---
+++ GCD_adv.py
@@ -0,0 +1,19 @@
+# Find the GCD of two numbers.
+
+# Euclidean Algorithm
+
+# GCD(a,b) = GCD(b,a%b)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter Second Number: "))
+
+# gcd(12,6) = gcd(6,12%6)
+
+def GCD(a:int,b:int)->int:
+
+    while b!=0: #as per the algo, loop will stop when b becomes 0 and we return a
+        a,b = b,a%b
+        
+    return a
+
+print(GCD(a,b))
